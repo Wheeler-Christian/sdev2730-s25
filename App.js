552d86e4f01f1f18ed944ba18b5ddd494596c2ea
Wheeler-Christian <christianwheeler505@gmail.com>
2023-12-20 15:38:04 -0700

    Start project

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.h1}>Select a Quantity</Text>
+      <Text style={styles.h2}>Number: 2</Text>
+      <View style={styles.buttonsContainer}>
+        <Button title="Add 1" color="green" />
+        <Button title="Remove 1" color="red" />
+        <Button title="Reset" color="gray" />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#111",
+    alignItems: "center",
+    // justifyContent: "center",
+    padding: 16,
+  },
+  h1: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  h2: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    marginTop: 24,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
   },
 });
---
+++ components/MyButton.js
@@ -0,0 +1,9 @@
+import { Pressable } from "react-native"
+
+function MyButton({}){
+    return <Pressable>
+
+    </Pressable>
+}
+
+export default MyButton
